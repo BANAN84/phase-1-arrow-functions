7bd6e8b1a1932f6e34dc67fc30d12691e379ea65
banan <banan.faouri2001@gmail.com>
2023-10-01 23:54:54 +0300

    done

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function (a,b){ return a/b ; };
+/*it("has an arrow function called add", () => {
+    expect(add).to.exist 
+    it("add arrow function takes two parameters and adds them together", () => {
+    let a = 3
+    let b = 4
+    expect(add(a,b)).to.equal(a+b)*/
+
+const square = x => x*x ;
+const add = (a,b) => a+b ;
